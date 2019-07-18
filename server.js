479f2088b4fdcae852e411bdfd450b7935362672
Abdel <abdelmess02400@hotmail.fr>
2019-07-18 12:15:42 +0200

    startGame

--- server.js
+++ server.js
@@ -3,16 +3,11 @@ const sockets = require('./sockets/sockets');
 const router = require('./routes/routes');
 const bodyParser = require('body-parser');
 const {app, game} = require('./config/config');
-const mongoose = require('mongoose');
-var carte = require('./middleware/carte.js');
-let Carte = require('./models/carte.js');
 
 
 app.use('/', router);
 app.use(express.static('static'));
 app.use(bodyParser.json());
-
-var PORT = 8080;
 
 game.on('connection', sockets.gameNamespace);
 
--- sockets/sockets.js
+++ sockets/sockets.js
@@ -12,8 +12,6 @@ mongoose.set('useFindAndModify', false);
 
 var gameNamespace = (socket) => {
 
-    socket.emit('message','Bienvenue sur le jeux de 1000 bornes');
-
     var getPlayersId = () => {
         return new Promise((resolve, reject) => {
             redisClient.smembers("joueurs", (err, result) => {
@@ -32,6 +30,154 @@ var gameNamespace = (socket) => {
         })
     }
 
+    var resetGame = () => {
+        return new Promise((resolve, reject) => {
+            redisClient.hgetall('game', (err, result) => {
+                if(result !== null) {
+                    redisClient.flushall( (err, result) => {
+                        if(err) reject(err);
+                        resolve(true);
+                    });
+                }else resolve(false)
+            });
+        })
+    }
+
+    var generateDeck = () => {
+        return new Promise((resolve, reject) => {
+            var deck = [];
+
+            findCardsType.then((Cards) => {
+                try{
+
+                    for(var i = 0; i < Cards.length; i++) {
+                        if(Cards[i].type === "Botte") {
+                            deck.push(Cards[i]);
+                        } // FIN BOTTE
+                        if(Cards[i].type === "Attaque") {
+                            if(Cards[i].nom === "panne" || Cards[i].nom === "accident" || Cards[i].nom === "crevaison") {
+                                for(var a = 0; a < 3; a++) {
+                                    deck.push(Cards[i]);
+                                }
+                            }
+                            else {
+                                if(Cards[i].nom === "limit") {
+                                    for(var a = 0; a < 4; a++) {
+                                        deck.push(Cards[i]);
+                                    }
+                                }
+                                if(Cards[i].nom === "feu_rouge"){
+                                    for(var a = 0; a < 5; a++) {
+                                        deck.push(Cards[i]);
+                                    }
+                                }
+                            }
+
+                        } // FIN ATTAQUE
+
+
+                        if(Cards[i].type === "Parade") {
+                            if(Cards[i].nom !== "feu_vert") {
+                                for(var p = 0; p < 6; p++) {
+                                    deck.push(Cards[i]);
+                                }
+                            }
+                            else {
+                                for(var p = 0; p < 14; p++) {
+                                    deck.push(Cards[i]);
+                                }
+                            }
+                        } // FIN PARADE
+
+
+                        if(Cards[i].type === "Distance") {
+                            if(Cards[i].nom === "25km" || Cards[i].nom === "50km" || Cards[i].nom === "75km") {
+                                for(var a = 0; a < 10; a++) {
+                                    deck.push(Cards[i]);
+                                }
+                            }
+                            else {
+                                if(Cards[i].nom === "100km") {
+                                    for(var a = 0; a < 12; a++) {
+                                        deck.push(Cards[i]);
+                                    }
+                                }
+                                if(Cards[i].nom === "200km") {
+                                    for(var a = 0; a < 4; a++) {
+                                        deck.push(Cards[i]);
+                                    }
+                                }
+                            }
+
+                        } // FIN DISTANCE
+
+
+                    }
+                    let countDistance = 0;
+                    let countBotte = 0;
+                    let countParade = 0;
+                    let countAttaque = 0;
+                    for(i = 0; i < deck.length; i++) {
+                        if(deck[i].type === "Distance") {
+
+                            countDistance++;
+                        }
+
+                        if(deck[i].type === "Botte") {
+
+                            countBotte++;
+                        }
+
+                        if(deck[i].type === "Parade") {
+
+                            countParade++;
+                        }
+
+                        if(deck[i].type === "Attaque") {
+
+                            countAttaque++;
+                        }
+
+                    };
+                    console.log("NOMBRE DISTANCE : " + countDistance);
+                    console.log("NOMBRE BOTTE : " + countBotte);
+                    console.log("NOMBRE PARADE : " + countParade);
+                    console.log("NOMBRE ATTAQUE : " + countAttaque);
+                    console.log("TAILLE DU DECK : " + deck.length);
+                    var ctr = deck.length, temp, index;
+
+                    // Mélange du tableau
+                    while (ctr > 0) {
+
+                        index = Math.floor(Math.random() * ctr);
+
+                        ctr--;
+
+                        temp = deck[ctr];
+                        deck[ctr] = deck[index];
+                        deck[index] = temp;
+                    }
+                    resolve(deck);
+
+                }
+                catch (e){
+                }
+
+            })
+        })
+    }
+
+    var findCardsType = new Promise(function(resolve, reject){
+
+        let Cards = Carte.find({}, function(err, cartesRetour) {
+            if(err) throw err;
+        }).lean();
+
+        resolve(Cards);
+    });
+
+    socket.emit('message','Bienvenue sur le jeux de 1000 bornes');
+
     redisClient.smembers("joueurs", (err, result) => {
         if(result.length >= 4) {
             socket.emit('complete', true);
@@ -42,6 +188,26 @@ var gameNamespace = (socket) => {
             socket.broadcast.emit('complete', false);
         }
     });
+
+    redisClient.hgetall('game', (err, result) => {
+        if(result === null) {
+            socket.emit('start', false);
+            socket.broadcast.emit('start', false);
+        }else {
+            socket.emit('start', true);
+            socket.broadcast.emit('start', true);
+        }
+    });
+
+    socket.on('start', async (data) => {
+        redisClient.hmset('game', "start", data);
+        socket.emit('start', true);
+        socket.broadcast.emit('start', true);
+        var deck = await generateDeck();
+        socket.emit('deck', deck);
+        socket.broadcast.emit('deck', deck);
+
+    })
 
 
     socket.on('addPlayer', async (data) => {
@@ -76,10 +242,8 @@ var gameNamespace = (socket) => {
             socket.broadcast.emit('complete', true);
         }
 
-        var deck = await generateDeck();
-
-        socket.emit('listPlayer', {players,deck});
-        socket.broadcast.emit('listPlayer', {players,deck});
+        socket.emit('listPlayer', {players});
+        socket.broadcast.emit('listPlayer', {players});
 
 
     });
@@ -97,6 +261,8 @@ var gameNamespace = (socket) => {
                 nbplayer = user.player;
                 redisClient.del(id);
                 redisClient.srem('joueurs', id);
+                var gameFinished = await resetGame();
+                if(gameFinished) socket.broadcast.emit('finishGame', true);
             }
 
         }
@@ -110,178 +276,22 @@ var gameNamespace = (socket) => {
             }
         }
 
-        /*redisClient.smembers('joueurs', (err, result) => {
-            result.forEach(function(playerId) {
-                if(playerId === id) {
-
-                    redisClient.hgetall(playerId, (err, result) => {
-                        nbplayer = result.player;
-                    });
-
-                    redisClient.del(playerId);
-                    redisClient.srem('joueurs', playerId);
-                }
-            });
-
-            redisClient.smembers("joueurs", (err, result) => {
-                result.forEach(function(playerId) {
-                    redisClient.hgetall(playerId, (err, result) => {
-                        if(result.player > nbplayer){
-                            var newResult = parseInt(result.player) -1;
-                            redisClient.hset(playerId, 'player', newResult);
-                        }
-                    });
-                })
-            });
-        });*/
-
-        var res2 = {};
+        var players = {};
         playersIds = await getPlayersId();
 
         for(var i=0; i< playersIds.length; i++) {
             user = await getUser(playersIds[i]);
-            res2[user.socketid] = {
+            players[user.socketid] = {
                 name: user.name,
                 score: user.score,
                 socketid: user.socketid,
                 player: user.player
             }
         }
-        socket.broadcast.emit('listPlayer', res2);
+
+
+        socket.broadcast.emit('listPlayer', {players});
     });
 }
-
-var generateDeck = () => {
-    return new Promise((resolve, reject) => {
-        var deck = [];
-
-        findCardsType.then((Cards) => {
-            try{
-
-                for(var i = 0; i < Cards.length; i++) {
-                    if(Cards[i].type === "Botte") {
-                        deck.push(Cards[i]);
-                    } // FIN BOTTE
-                    if(Cards[i].type === "Attaque") {
-                        if(Cards[i].nom === "panne" || Cards[i].nom === "accident" || Cards[i].nom === "crevaison") {
-                            for(var a = 0; a < 3; a++) {
-                                deck.push(Cards[i]);
-                            }
-                        }
-                        else {
-                            if(Cards[i].nom === "limit") {
-                                for(var a = 0; a < 4; a++) {
-                                    deck.push(Cards[i]);
-                                }
-                            }
-                            if(Cards[i].nom === "feu_rouge"){
-                                for(var a = 0; a < 5; a++) {
-                                    deck.push(Cards[i]);
-                                }
-                            }
-                        }
-
-                    } // FIN ATTAQUE
-
-
-                    if(Cards[i].type === "Parade") {
-                        if(Cards[i].nom !== "feu_vert") {
-                            for(var p = 0; p < 6; p++) {
-                                deck.push(Cards[i]);
-                            }
-                        }
-                        else {
-                            for(var p = 0; p < 14; p++) {
-                                deck.push(Cards[i]);
-                            }
-                        }
-                    } // FIN PARADE
-
-
-                    if(Cards[i].type === "Distance") {
-                        if(Cards[i].nom === "25km" || Cards[i].nom === "50km" || Cards[i].nom === "75km") {
-                            for(var a = 0; a < 10; a++) {
-                                deck.push(Cards[i]);
-                            }
-                        }
-                        else {
-                            if(Cards[i].nom === "100km") {
-                                for(var a = 0; a < 12; a++) {
-                                    deck.push(Cards[i]);
-                                }
-                            }
-                            if(Cards[i].nom === "200km") {
-                                for(var a = 0; a < 4; a++) {
-                                    deck.push(Cards[i]);
-                                }
-                            }
-                        }
-
-                    } // FIN DISTANCE
-
-
-                }
-                let countDistance = 0;
-                let countBotte = 0;
-                let countParade = 0;
-                let countAttaque = 0;
-                for(i = 0; i < deck.length; i++) {
-                    if(deck[i].type === "Distance") {
-
-                        countDistance++;
-                    }
-
-                    if(deck[i].type === "Botte") {
-
-                        countBotte++;
-                    }
-
-                    if(deck[i].type === "Parade") {
-
-                        countParade++;
-                    }
-
-                    if(deck[i].type === "Attaque") {
-
-                        countAttaque++;
-                    }
-
-                };
-                console.log("NOMBRE DISTANCE : " + countDistance);
-                console.log("NOMBRE BOTTE : " + countBotte);
-                console.log("NOMBRE PARADE : " + countParade);
-                console.log("NOMBRE ATTAQUE : " + countAttaque);
-                console.log("TAILLE DU DECK : " + deck.length);
-                var ctr = deck.length, temp, index;
-
-                // Mélange du tableau
-                while (ctr > 0) {
-
-                    index = Math.floor(Math.random() * ctr);
-
-                    ctr--;
-
-                    temp = deck[ctr];
-                    deck[ctr] = deck[index];
-                    deck[index] = temp;
-                }
-                resolve(deck);
-
-            }
-            catch (e){
-            }
-
-        })
-    })
-}
-
-var findCardsType = new Promise(function(resolve, reject){
-
-    let Cards = Carte.find({}, function(err, cartesRetour) {
-        if(err) throw err;
-    }).lean();
-
-    resolve(Cards);
-});
 
 module.exports = { gameNamespace }
