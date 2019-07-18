1ecfa74d0a509934725173b05a58d3c601a588d2
Gwendal <gwendal.nunez@hotmail.fr>
2019-07-18 17:48:39 +0200

    Ajout méthodes gestion effets cartes

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -66,15 +66,26 @@ var gameNamespace = (socket) => {
         }
     });
 
-    /*socket.on('eventCard', async (data) => {
+    socket.on('eventCard', async (data) => {
         var data = {
-            expediteur: socket.id;
-            destinataire: socket.id || corbeille;
-            cardElement: {
-                ...
-            }
+            expediteur: socket.id,
+            destinataire: socket.id,
+            cardElement: card,
+        };
+        var isMovePlayable = false;
+        switch (data.cardElement.type) {
+          case "Distance":
+            isMovePlayable = playDistance(data.cardElement, data.expediteur);
+          case "Botte":
+            isMovePlayable = playBotte(card.cardElement, data.expediteur);
+          case "Attaque":
+            isMovePlayable = playAttaque(card.cardElement, data.expediteur, data.destinataire);
+          case "Parade":
+            isMovePlayable = playParade(card.cardElement, data.expediteur);
         }
-    })*/
+        socket.emit('playJoueur', isMovePlayable); // RETOUR AU FRONT
+      }
+    );
 
     socket.on('start', async (data) => {
         redisClient.hmset('game', "start", data);
@@ -82,14 +93,16 @@ var gameNamespace = (socket) => {
         socket.broadcast.emit('start', true);
 
         var deck = await generateDeck();
+        var trash = [];
         var playersIds = await getPlayersId();
 
         var result;
         // Distribution des cartes premier tour
         for (var i = 0; i < playersIds.length; i++) {
-            result = await distributeCards(6, playersIds[i], deck);
+            result = await distributeCards(6, playersIds[i], deck, trash);
         }
-
+        deck = result.deck;
+        trash = result.trash;
         socket.emit('deck', result);
         socket.broadcast.emit('deck', result);
 
@@ -180,17 +193,126 @@ var gameNamespace = (socket) => {
     });
 }
 
-function checkVictory(player) {
-  if (player.score == 1000)
-    socket.broadcast.emit('win', player);
-}
 
-var distributeCards = (numberCards, playerID, deck) => {
+function playDistance(card, expediteur) {
+    var isMovePlayable = false;
+    switch (card.nom) {
+      case "25km": {
+        if (expediteur.score + 25 <= 1000 && expediteur.alteration == "") {
+            expediteur.score += 25;
+          isMovePlayable = true;
+        }
+      }
+      case "50km": {
+        if (expediteur.score + 50 <= 1000 && expediteur.alteration == "") {
+            expediteur.score += 50;
+          isMovePlayable = true;
+        }
+      }
+      case "75km": {
+        if (expediteur.score + 75 <= 1000 && (expediteur.alteration == "" || expediteur.alteration != "limit")) {
+            expediteur.score += 75;
+          isMovePlayable = true;
+        }
+      }
+      case "100km": {
+        if (expediteur.score + 100 <= 1000 && (expediteur.alteration == "" || expediteur.alteration != "limit")) {
+            expediteur.score += 100;
+          isMovePlayable = true;
+        }
+      }
+      case "200km": {
+        if (expediteur.score + 200 <= 1000 && (expediteur.alteration == "" || expediteur.alteration != "limit")) {
+            expediteur.score += 200;
+          isMovePlayable = true;
+        }
+      }
+    }
+    return (isMovePlayable);
+  }
+  
+  function playParade(card, expediteur) {
+    var isMovePlayable = false;
+    switch (card.nom) {
+      case "essence": {
+        if (expediteur.alteration == "panne")
+          isMovePlayable = true;
+      }
+      case "fin_limit": {
+        if (expediteur.alteration == "limit")
+          isMovePlayable = true;
+      }
+      case "feu_vert": {
+        if (expediteur.alteration == "feu_rouge")
+          isMovePlayable = true;
+      }
+      case "reparations": {
+        if (expediteur.alteration == "accident")
+          isMovePlayable = true;
+      }
+      case "roue": {
+        if (expediteur.alteration == "crevaison")
+          isMovePlayable = true;
+      }
+    }
+    return (isMovePlayable);
+  }
+  
+  function playBotte(card, expediteur) {
+    expediteur.bottes.push(card.nom);
+    return true;
+  }
+  
+  function playAttaque(card, expediteur, destinataire) {
+    var isMovePlayable = false;
+    switch (carte.nom) {
+      case "panne": {
+        if (!destinataire.bottes.includes("citerne") && destinataire.alteration == "") {
+            destinataire.alteration = carte.nom;
+          isMovePlayable = true;
+        }
+      }
+      case "limit": {
+        if (!destinataire.bottes.includes("prioritaire") && destinataire.alteration == "") {
+            destinataire.alteration = carte.nom;
+          isMovePlayable = true;
+        }
+      }
+      case "feu_rouge": {
+        if (!destinataire.bottes.includes("prioritaire") && destinataire.alteration == "") {
+            destinataire.alteration = carte.nom;
+          isMovePlayable = true;
+        }
+      }
+      case "accident": {
+        if (!destinataire.bottes.includes("as_volant") && destinataire.alteration == "") {
+            destinataire.alteration = carte.nom;
+          isMovePlayable = true;
+        }
+      }
+      case "crevaison": {
+        if (!destinataire.bottes.includes("increvable") && destinataire.alteration == "") {
+            destinataire.alteration = carte.nom;
+          isMovePlayable = true;
+        }
+      }
+    }
+    return (isMovePlayable);
+  }
+  
+  
+  function checkVictory(player) {
+    if (player.score == 1000)
+      socket.broadcast.emit('win', player);
+  }
+
+var distributeCards = (numberCards, playerID, deck, trash) => {
     return new Promise((resolve, reject) =>{
         var cardsElt = [];
         for(var i = 0; i < numberCards; i++)
         {
             cardsElt.push(deck[deck.length - 1]);
+            trash.push(deck[deck.length - 1 ]);
             deck.pop();
         }
         var main;
@@ -200,7 +322,8 @@ var distributeCards = (numberCards, playerID, deck) => {
             socketid: playerID,
         }
         var result = {main : main
-            ,deck : deck};
+            ,deck : deck,
+            trash: trash};
 
         resolve(result);
     })
